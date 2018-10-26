fb73fb5cebfa9d0f9bae03ec37c9b22fd05655be
Ronny Weiss <30749177+RonnyWeiss@users.noreply.github.com>
2018-10-26 21:13:49 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# APEX-Copy-Text-2-Clipboard
+ # APEX-Copy-Text-2-Clipboard
+
+![Screenshot](https://github.com/RonnyWeiss/APEX-Copy-Text-2-Clipboard/blob/master/screenshot.png?raw=true)
+
+![Screenshot](https://github.com/RonnyWeiss/APEX-Copy-Text-2-Clipboard/blob/master/screenshot.gif?raw=true)
+
 This dynamic action plug-in is used to copy any text or the value of an item to clipboard when click on the text or on a button...
+
+For working Demo just click on:
+
+https://apex.oracle.com/pls/apex/f?p=103428
+
+Login is: user-demo / 123456@
+
+If you like my stuff, donate me a coffee
+
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.me/RonnyW1)
---
+++ js/script.js
@@ -0,0 +1,30 @@
+function apexCopy2ClipBoard(self, wait, message, strLength) {
+    var util = {
+        version: "1.0.2",
+        cutString: function (text, textLength) {
+            try {
+                if (textLength < 0) return text;
+                else {
+                    return (text.length > textLength) ?
+                        text.substring(0, textLength - 3) + "..." :
+                        text
+                }
+            } catch (e) {
+                return text;
+            }
+        }
+    };
+
+    var $temp = $("<input>");
+    $("body").append($temp);
+    var str = $(self.affectedElements).text() || $(self.affectedElements).val();
+    $temp.val(str).select();
+    document.execCommand("copy");
+    $temp.remove();
+    if ((wait || 0) > 0 && str) {
+        apex.message.showPageSuccess(message.replace("%0", util.cutString(str, strLength || 15)));
+        setTimeout(function () {
+            apex.message.hidePageSuccess();
+        }, wait);
+    }
+}
